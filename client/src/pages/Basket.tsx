0a11ee3a6af0546e2aa6bc648489b1d33efdb6cd
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-10-11 17:00:41 +0900

    Feat: Create order using socket

--- client/src/pages/Basket.tsx
+++ client/src/pages/Basket.tsx
@@ -1,26 +1,16 @@
+import { useContext, useEffect, useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
 import Layout from "../components/Layout/Layout";
-import { AiOutlineArrowLeft } from "react-icons/ai";
+import { SocketContext } from "../contexts/SocketContext";
 import { styles } from "../utils/style";
 
 function Basket() {
-  const navigate = useNavigate();
   return (
     <Layout>
       <Header />
       <Body />
-      <button
-        onClick={() => navigate("/result")}
-        className={`min-w-[336px] w-[336px] fixed ${styles.footerHeight} h-20 flex flex-col justify-center items-center`}
-      >
-        <button className="w-full h-10 text-white bg-[#FF385C]">
-          총 22% 할인받고 구매하기
-        </button>
-        <div className="flex justify-center items-center h-10 bg-white w-full">
-          총액: 14,000원
-        </div>
-      </button>
       <Footer />
     </Layout>
   );
@@ -44,82 +34,175 @@ function Header() {
   );
 }
 
+const items = [
+  {
+    itemId: 1,
+    storeId: 1,
+    title: "종훈 떡볶이",
+    count: 1,
+    prevPrice: 4000,
+    price: 3000,
+    imgUrl: "",
+  },
+  {
+    itemId: 2,
+    storeId: 1,
+    title: "종훈 떡볶이",
+    count: 2,
+    prevPrice: 4000,
+    price: 3000,
+    imgUrl: "",
+  },
+  {
+    itemId: 3,
+    storeId: 1,
+    title: "종훈 떡볶이",
+    count: 3,
+    prevPrice: 4000,
+    price: 3000,
+    imgUrl: "",
+  },
+  {
+    itemId: 4,
+    storeId: 1,
+    title: "종훈 떡볶이",
+    count: 4,
+    prevPrice: 4000,
+    price: 3000,
+    imgUrl: "",
+  },
+  {
+    itemId: 5,
+    storeId: 1,
+    title: "종훈 떡볶이",
+    count: 5,
+    prevPrice: 4000,
+    price: 3000,
+    imgUrl: "",
+  },
+  {
+    itemId: 6,
+    storeId: 1,
+    title: "종훈 떡볶이",
+    count: 6,
+    prevPrice: 4000,
+    price: 3000,
+    imgUrl: "",
+  },
+];
+
 function Body() {
-  const items = [
-    {
-      title: "종훈 떡볶이",
-      content: "매콤 달달 떡볶이, 1,000원 할인!",
-      count: 12,
-      prevPrice: 4000,
-      price: 3000,
-      imgUrl: "",
-    },
-    {
-      title: "종훈 떡볶이",
-      content: "매콤 달달 떡볶이, 1,000원 할인!",
-      count: 12,
-      prevPrice: 4000,
-      price: 3000,
-      imgUrl: "",
-    },
-    {
-      title: "종훈 떡볶이",
-      content: "매콤 달달 떡볶이, 1,000원 할인!",
-      count: 12,
-      prevPrice: 4000,
-      price: 3000,
-      imgUrl: "",
-    },
-    {
-      title: "종훈 떡볶이",
-      content: "매콤 달달 떡볶이, 1,000원 할인!",
-      count: 12,
-      prevPrice: 4000,
-      price: 3000,
-      imgUrl: "",
-    },
-    {
-      title: "종훈 떡볶이",
-      content: "매콤 달달 떡볶이, 1,000원 할인!",
-      count: 12,
-      prevPrice: 4000,
-      price: 3000,
-      imgUrl: "",
-    },
-    {
-      title: "종훈 떡볶이",
-      content: "매콤 달달 떡볶이, 1,000원 할인!",
-      count: 12,
-      prevPrice: 4000,
-      price: 3000,
-      imgUrl: "",
-    },
-  ];
+  const socket = useContext(SocketContext);
+  const navigate = useNavigate();
+
+  type ItemCountsType = { [key: number]: number };
+
+  const [itemCounts, setItemCounts] = useState<ItemCountsType>(() => {
+    const initialCounts: ItemCountsType = {};
+    items.forEach((item) => {
+      initialCounts[item.itemId] = item.count;
+    });
+    return initialCounts;
+  });
+
+  useEffect(() => {
+    if (socket) {
+      const handleOrderAlarm = (data: any) => {
+        console.log(data);
+      };
+
+      socket.on("orderAlarmToOwner", handleOrderAlarm);
+      return () => {
+        socket.off("orderAlarmToOwner", handleOrderAlarm);
+      };
+    }
+  }, [socket]);
+
+  const incrementCount = (itemId: number) => {
+    setItemCounts((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] + 1,
+    }));
+  };
+
+  const decrementCount = (itemId: number) => {
+    if (itemCounts[itemId] > 1) {
+      setItemCounts((prev) => ({
+        ...prev,
+        [itemId]: prev[itemId] - 1,
+      }));
+    }
+  };
+
+  let totalPrice = 0;
+  let totalPrevPrice = 0;
+  items.forEach((item) => {
+    totalPrice += item.price * itemCounts[item.itemId];
+    totalPrevPrice += item.prevPrice * itemCounts[item.itemId];
+  });
+  const discountPercentage = Math.round(
+    (1 - totalPrice / totalPrevPrice) * 100
+  );
+
+  const sendOrder = () => {
+    if (socket) {
+      const itemList: { [key: number]: number } = {};
+      items.forEach((item) => {
+        itemList[item.itemId] = itemCounts[item.itemId];
+      });
+
+      socket.emit("clientOrder", {
+        userId: 6,
+        storeId: items[0].storeId,
+        totalPrice: totalPrice,
+        discount: discountPercentage,
+        itemList: itemList,
+      });
+    }
+  };
+
   return (
-    <div className={`w-full ${styles.headerMargin} mb-32 overflow-auto`}>
-      {items.map((item: any, index: number) => (
-        <div
-          className={`w-full h-[180px] flex flex-col p-2 border-b-2 border-[#C3CFD9] pt-4       
+    <>
+      <div className={`w-full ${styles.headerMargin} mb-32 overflow-auto`}>
+        {items.map((item: any, index: number) => (
+          <div
+            key={item.itemId}
+            className={`w-full h-[180px] flex flex-col p-2 border-b-2 border-[#C3CFD9] pt-4       
           ${index % 2 === 0 ? "bg-[#F7F9FA]" : "bg-white"}
         `}
-        >
-          <div className="px-2">{item.title}</div>
-          <div className="flex h-full justify-around items-center">
-            <div className="h-[100px] w-[100px] rounded-md flex justify-center items-center bg-blue-400">
-              사진
-            </div>
-            <div className="h-full w-[30%] flex flex-col justify-center">
-              <div>가격: {item.price}원</div>
-              <div>{item.price}원</div>
-            </div>
-            <div className="flex gap-2 py-1 px-2 items-center">
-              <button>-</button>
-              <div>0</div>
-              <button>+</button>
+          >
+            <div className="px-2">{item.title}</div>
+            <div className="flex h-full justify-around items-center">
+              <div className="h-[100px] w-[100px] rounded-md flex justify-center items-center bg-blue-400">
+                사진
+              </div>
+              <div className="h-full w-[30%] flex flex-col justify-center">
+                <div>가격: {item.price}원</div>
+                <div>{item.price}원</div>
+              </div>
+              <div className="flex gap-2 py-1 px-2 items-center">
+                <button onClick={() => decrementCount(item.itemId)}>-</button>
+                <div>{itemCounts[item.itemId]}</div>
+                <button onClick={() => incrementCount(item.itemId)}>+</button>
+              </div>
             </div>
           </div>
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          sendOrder();
+          navigate("/result");
+        }}
+        className={`min-w-[336px] w-[336px] fixed ${styles.footerHeight} h-20 flex flex-col justify-center items-center`}
+      >
+        <button className="w-full h-10 text-white bg-[#FF385C]">
+          총 {discountPercentage}% 할인받고 구매하기
+        </button>
+        <div className="flex justify-center items-center h-10 bg-white w-full">
+          총액: {totalPrice}원
         </div>
-      ))}
-    </div>
+      </button>
+    </>
   );
 }
--- client/src/pages/Notification.tsx
+++ client/src/pages/Notification.tsx
@@ -1,9 +1,7 @@
-import { useContext, useEffect } from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
 import Layout from "../components/Layout/Layout";
-import { SocketContext } from "../contexts/SocketContext";
 import { styles } from "../utils/style";
 
 function Notification() {
@@ -35,29 +33,6 @@ function Header() {
 
 function Body() {
   const navigate = useNavigate();
-  const socket = useContext(SocketContext);
-
-  useEffect(() => {
-    if (socket) {
-      socket.emit("clientOrder", {
-        userId: 1,
-        storeId: 2,
-        totalPrice: 14000,
-        discount: 22,
-        itemList: {
-          1: 4,
-          2: 6,
-          3: 9,
-        },
-      });
-    }
-
-    return () => {
-      if (socket) {
-        socket.off();
-      }
-    };
-  }, [socket]);
 
   const alarms = [
     {
