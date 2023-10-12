684a1ad16ef5d10510ebd5a84eff37fd3a98291d
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-10-12 11:05:10 +0900

    Feat: Fetch store data and inject to owner store component

--- client/src/pages/OwnerStore.tsx
+++ client/src/pages/OwnerStore.tsx
@@ -1,5 +1,7 @@
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchStoreById } from "../api/storeAPI";
 import Layout from "../components/Layout/Layout";
 import OwnerFooter from "../components/Layout/OwnerFooter";
 import { styles } from "../utils/style";
@@ -34,29 +36,37 @@ function Header() {
 }
 
 function Body() {
+  const { id } = useParams();
+  const { data: store, isLoading } = useQuery(["store", id], () =>
+    fetchStoreById(id!)
+  );
+  console.log(store);
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
   return (
     <div className={`flex flex-col ${styles.headerMargin} p-6 gap-2`}>
       <div className="font-semibold text-[1.25rem]">영업 정보</div>
-      <div className="flex">
+      <div className="flex gap-2">
         <div className="w-[100px]">상호명</div>
-        <div>종훈 떡볶이</div>
+        <div className="w-full">{store.store.name}</div>
       </div>
-      <div className="flex">
+      <div className="flex gap-2">
         <div className="w-[100px]">주소</div>
-        <div>서울 특별시 테헤란로 77</div>
+        <div className="w-full">{store.store.address}</div>
       </div>
-      <div className="flex">
+      <div className="flex gap-2">
         <div className="w-[100px]">전화번호</div>
-        <div>010-4499-3894</div>
+        <div className="w-full">{store.store.storePhoneNumber}</div>
       </div>
-      <div className="flex">
+      <div className="flex gap-2">
         <div className="w-[100px]">업종</div>
-        <div>음식점</div>
+        <div className="w-full">{store.store.category}</div>
       </div>
-      <div className="flex">
+      {/* <div className="flex gap-2">
         <div className="w-[100px]">관리번호</div>
-        <div>300000-235145-458411</div>
-      </div>
+        <div className="w-full">{store.store.managementNumber}</div>
+      </div> */}
     </div>
   );
 }
