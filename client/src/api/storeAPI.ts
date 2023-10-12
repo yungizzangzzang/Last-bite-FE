e6a8a17e1785abc001d3ff9683b919e0e841c583
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-10-12 10:08:04 +0900

    Feat: Add fetch reviews feature

--- client/src/api/storeAPI.ts
+++ client/src/api/storeAPI.ts
@@ -4,3 +4,8 @@ export const fetchStoreById = async (storeId: string) => {
   const response = await getAPI(`/stores/${storeId}`);
   return response.data.data;
 };
+
+export const fetchReviewsByStoreId = async (storeId: string) => {
+  const response = await getAPI(`/reviews/${storeId}`);
+  return response.data.data;
+};
--- client/src/pages/Review.tsx
+++ client/src/pages/Review.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import { GoHeartFill } from "react-icons/go";
-import { useNavigate } from "react-router-dom";
+import { GoHeart, GoHeartFill } from "react-icons/go";
+import { useMutation, useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchReviewsByStoreId, fetchStoreById } from "../api/storeAPI";
+import { postAPI } from "../axios";
 import Footer from "../components/Layout/Footer";
 import Layout from "../components/Layout/Layout";
 import { styles } from "../utils/style";
 
 function Review() {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const { data: store, isLoading } = useQuery(["store", id], () =>
+    fetchStoreById(id!)
+  );
+
+  const { data: reviews, isLoading: reviewsIsLoading } = useQuery(
+    ["reviews", id],
+    () => fetchReviewsByStoreId(id!)
+  );
+  console.log(store, reviews);
+  if (isLoading) return <div>Loading...</div>;
   return (
     <>
       <Layout>
-        <Header />
+        <Header storeId={id!} store={store.store} />
         <Body />
         <div className="min-w-[336px] w-[336px] fixed bottom-[50px] h-12 flex justify-center items-center">
           <button
@@ -28,19 +43,30 @@ function Review() {
 
 export default Review;
 
-function Header() {
+function Header({ storeId, store }: { storeId: string; store: any }) {
   const navigate = useNavigate();
+  const [isLiked, setIsLiked] = useState(store.isLiked);
+
+  const mutation = useMutation(() => postAPI(`/likes/${storeId}`, {}), {
+    onSuccess: () => {
+      setIsLiked(!isLiked);
+    },
+  });
+
+  const toggleLike = () => {
+    mutation.mutate();
+  };
+
   return (
     <div className={styles.header}>
-      <div
-        onClick={() => {
-          navigate(-1);
-        }}
-        className="px-2 flex gap-2 items-center"
-      >
-        <AiOutlineArrowLeft />
-        <span> 종훈 떡볶이</span>
-        <GoHeartFill color="#FF5352" />
+      <div className="px-2 flex gap-2 items-center">
+        <AiOutlineArrowLeft onClick={() => navigate(-1)} />
+        <span> {store.name}</span>
+        {isLiked ? (
+          <GoHeartFill color="#FF5352" onClick={toggleLike} />
+        ) : (
+          <GoHeart onClick={toggleLike} />
+        )}
       </div>
     </div>
   );
